b3304797eb2c4eb3d175941e2f1026af84be966f
Celeste Layne <layne.celeste@gmail.com>
2022-05-07 14:54:27 -0400

    begin styling the searchbar

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import styled from 'styled-components'
+
+const Input = styled.input.attrs({
+  type: 'text',
+})`
+  background: #F4F4F4;
+  height: 40px;
+  width: 100%;
+  font-size: 18px;
+  border: none;
+  cursor: pointer;
+  ::placeholder {
+    color: #141414;
+  },
+  :focus {
+    outline: none;
+  }
+`
+
 
 const Searchbar = ({ searchTerm, handleSearchChange }) => {
+
+  // console.log(searchTerm)
+  
   return(
     <div>
-      <input
-        type="text"
+      <Input
+        type="search"
         value={searchTerm}
         onChange={handleSearchChange}
         placeholder="Search"
