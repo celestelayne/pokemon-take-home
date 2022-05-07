3f8ef6575f79e332cc375c49218caaefcc560e7b
Celeste Layne <layne.celeste@gmail.com>
2022-05-07 15:04:07 -0400

    move favorite button to character item

--- src/components/CharacterItem.js
+++ src/components/CharacterItem.js
@@ -1,32 +1,66 @@
 import React from 'react';
-
 import {
   Link
 } from "react-router-dom";
 import styled from 'styled-components'
 
+import FavoriteBtn from './FavoriteBtn'
+
 const CharacterCards = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  padding: 5px;
-  flex-basis: auto;
+  flex: 1 0 20%;
+  margin: 5px;
+  min-height: 100%;
+  border: 1px solid black;
 `
 
-const CharacterItem = ({character, showOneCharacter}) => {
-  // console.log(character)
-  if (character !== undefined) {
-    return (
-      <CharacterCards>
+const Header = styled.div`
+  height: 50vh;
+  padding: 20px;
+`
+
+const Image = styled.img`
+  height: 100%;
+`
+
+const Footer = styled.div`
+  height: 15vh;
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  background: lemonchiffon;
+`
+
+const CharacterItem = ({
+  character, 
+  handleDetailsClick, 
+  isFavorite, 
+  toggleFavorite 
+}) => {
+
+  if (!character) {
+    // maybe loader
+    return null;
+  }
+
+  let image = `https://img.pokemondb.net/artwork/${character.name.toLowerCase().replace(/[&\\/\\\\#,+()$~%.'":*?<>{}]/g, '').replace(' ', '-')}.jpg`;
+
+  return (
+    <CharacterCards className="character-card">
+      <Header className="character-header">
+        <Image src={image} />
+      </Header>
+      <Footer className="character-footer">
         <Link 
           to={`/${character.name}`}
-          onClick={() => showOneCharacter(character)}
+          onClick={() => handleDetailsClick(character)}
         >{character.name}</Link>
-      </CharacterCards>
-    )
-  }
-  
-  
-
+        <p>{character.types}</p>
+        <FavoriteBtn 
+          onClick={() => toggleFavorite(character.id)}
+          favorite={isFavorite(character.id)} />
+          </Footer>
+    </CharacterCards>
+  )
 }
 
 export default CharacterItem;
