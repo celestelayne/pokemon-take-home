1e4bfc9230a4dce7950f1e9b0bc07eb27662208a
Celeste Layne <layne.celeste@gmail.com>
2022-05-07 14:56:00 -0400

    add children to the detail page

--- src/components/CharacterDetailPage.js
+++ src/components/CharacterDetailPage.js
@@ -1,15 +1,50 @@
 import React from 'react';
+import styled from 'styled-components'
 
-function CharacterDetailPage({currentCharacter}) {
+import { FaPlayCircle } from 'react-icons/fa';
 
-  console.log(currentCharacter);
+const Content = styled.div`
+  height: 20vh;
+  background: yellow;
+`
+
+const Footer = styled.div`
+  height: 10vh;
+  display: flex;
+`
+
+const Image = styled.div`
+  height: 60vh;
+`
+
+function CharacterDetailPage({ currentCharacter, children }) {
 
   let image = `https://img.pokemondb.net/artwork/${currentCharacter.name.toLowerCase().replace(/[&\\/\\\\#,+()$~%.'":*?<>{}]/g, '').replace(' ', '-')}.jpg`;
 
   return(
-    <>
-      <img src={image} />
-    </>
+    <div className="character-details">
+      <Image className="character-header">
+        <img src={image} />
+        <div className="character-sound">
+          <FaPlayCircle/>
+        </div>
+      </Image>
+      <Content className="character-content">
+        <p>{currentCharacter.name}</p>
+        <p>{currentCharacter.types}</p>
+      </Content>
+      <Footer className="character-footer">
+        <div>
+          <p>Weight</p>
+          <p>{currentCharacter.weight.minimum} &mdash; {currentCharacter.weight.maximum}</p>
+        </div>
+        <div>
+          <p>Height</p>
+          <p>{currentCharacter.height.minimum} &mdash; {currentCharacter.height.maximum}</p>
+        </div>
+      </Footer>
+      {children}
+    </div>
   )
 }
 
