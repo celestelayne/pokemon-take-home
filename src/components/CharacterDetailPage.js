f88aaa0fda95615a73ffd673c5fbdba9ba009b49
Celeste Layne <layne.celeste@gmail.com>
2022-05-10 22:57:30 -0400

    update classnames for styling

--- src/components/CharacterDetailPage.js
+++ src/components/CharacterDetailPage.js
@@ -4,17 +4,19 @@ import styled from 'styled-components'
 import { FaPlayCircle } from 'react-icons/fa';
 
 const Content = styled.div`
-  height: 20vh;
-  background: yellow;
+  height: 15vh;
+  background: #f3f3f3;
 `
 
 const Footer = styled.div`
   height: 10vh;
   display: flex;
+  background: #f3f3f3;
 `
 
 const Image = styled.div`
   height: 60vh;
+  object-fit: cover;
 `
 
 function CharacterDetailPage({ currentCharacter, children }) {
@@ -29,21 +31,40 @@ function CharacterDetailPage({ currentCharacter, children }) {
           <FaPlayCircle/>
         </div>
       </Image>
+
       <Content className="character-content">
-        <p>{currentCharacter.name}</p>
-        <p>{currentCharacter.types}</p>
+        <div className="character-content-top">
+          <div className="character-text">
+            <h2 className="character-title">{currentCharacter.name}</h2>
+            <p className="character-type">{currentCharacter.types}</p>
+          </div>
+          <div className="character-icon">
+            <span className="favorite-icon">{children}</span>
+          </div>
+        </div>
+        <div className="character-content-bottom">
+          <div className="combat">
+            <div className="combat-power"></div>
+            <p className="combat-power-score">CP: {currentCharacter.maxCP}</p>
+          </div>
+          <div className="hit">
+            <div className="hit-power"></div>
+            <p className="hit-power-score">HP: {currentCharacter.maxHP}</p>
+          </div>
+        </div>
       </Content>
+      
       <Footer className="character-footer">
-        <div>
-          <p>Weight</p>
+        <div className="character-footer-left">
+          <h3>Weight</h3>
           <p>{currentCharacter.weight.minimum} &mdash; {currentCharacter.weight.maximum}</p>
         </div>
-        <div>
-          <p>Height</p>
+        <div className="character-footer-right">
+          <h3>Height</h3>
           <p>{currentCharacter.height.minimum} &mdash; {currentCharacter.height.maximum}</p>
         </div>
       </Footer>
-      {children}
+
     </div>
   )
 }
