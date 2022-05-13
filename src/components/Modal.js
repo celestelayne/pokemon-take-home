21eb4db1f408439bea388b4bf1d60230cdeb1a30
Celeste Layne <layne.celeste@gmail.com>
2022-05-12 20:58:53 -0400

    add quick view modal

---
+++ src/components/Modal.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ViewModal = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Button = styled.button`
+  border: none;
+  background-color: black;
+  color: white;
+  cursor: pointer;
+  margin: 10px auto;
+  padding: 15px 30px;
+  text-transform: uppercase;
+  text-align: center;
+`;
+
+function Modal({ showModal, onClose, modalCharacter }) {
+  
+  if(!showModal) return null;
+
+  return (
+    <ViewModal className="modal">
+      <div className="modal-content">
+        <div className="modal-header">
+          <h2>{modalCharacter.name}</h2>
+          <p>
+            {modalCharacter.types.map((type, index) => {
+              return <span className="character-list-type" key={index}>{type},</span>
+            })} 
+          </p>
+        </div>
+        <div className="modal-body">
+          <p>Weight: {modalCharacter.weight.minimum} &mdash; {modalCharacter.weight.maximum}</p>
+          <p>Height: {modalCharacter.height.minimum} &mdash; {modalCharacter.height.maximum}</p>
+          <div className="combat">
+            <div className="combat-power"></div>
+            <p className="combat-power-score">CP: {modalCharacter.maxCP}</p>
+          </div>
+          <div className="hit">
+            <div className="hit-power"></div>
+            <p className="hit-power-score">HP: {modalCharacter.maxHP}</p>
+          </div>
+          <div className="resistant">
+            <p>Resistant: 
+              {modalCharacter.resistant.map((item, index) => {
+                return <span className="character-list-type" key={index}> {item},</span>
+              })} 
+            </p>
+          </div>
+          <div className="weaknesses">
+            <p>Weaknesses: 
+              {modalCharacter.weaknesses.map((weakness, index) => {
+                return <span className="character-list-type" key={index}> {weakness},</span>
+              })} 
+            </p>
+          </div>
+          <div className="attacks">
+            <p>Fast Attacks: 
+              {modalCharacter.attacks.fast.map((attack, index) => {
+                return <span className="character-list-type" key={index}> {attack.name},</span>
+              })} 
+            </p>
+          </div>
+
+          <div className="attacks">
+            <p>Special Attacks: 
+              {modalCharacter.attacks.special.map((attack, index) => {
+                return <span className="character-list-type" key={index}> {attack.name},</span>
+              })} 
+            </p>
+          </div>
+
+        </div>
+        <div className="modal-footer">
+          <Button className="modal-button" onClick={onClose}>Close</Button>
+        </div>
+      </div>
+    </ViewModal>
+  )
+}
+
+export default Modal;
